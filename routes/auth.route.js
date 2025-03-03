c0e67e5851a0b926bdf0e0321088213610cf4b65
muhammadale5 <muhammadale545@gmail.com>
2025-03-03 23:43:14 +0500

    added more code to auth route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+// console.log("Hello World");
+// // ***** Job Listings App
+
+// CRUD
+// C -> Create Jobs and Users
+// R -> Show All Jobs and Users
+// U -> Update Jobs and Users
+// D -> Delete Jobs and Users
+
+// Schema
+// Jobs
+// Users [Admin, User]
+
+// Admin [Create, Update and Delete]
+// User [Read or Search]
